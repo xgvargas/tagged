1142c9ca0c3b4eb28fb15bba6bd8f2bc0b1967b2
Gustavo Vilela Vargas <xgvargas@gmail.com>
2016-06-14 21:15:24 -0300

    jade to pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp       = require('gulp');
 var coffee     = require('gulp-coffee');
 var sass       = require('gulp-sass');
-var jade       = require('gulp-jade');
 var sh         = require('shelljs');
 var sourcemaps = require('gulp-sourcemaps');
-var svgng      = require('gulp-svg-ngmaterial');
 var gutil      = require('gulp-util');
 var uglify     = require('gulp-uglify');
+var yaml       = require('gulp-yaml');
+var pug        = require('gulp-pug');
 var ngAnnotate = require('gulp-ng-annotate');
+var svgng      = require('gulp-svg-ngmaterial');
 var svgmin     = require('gulp-svgmin');
 
 
@@ -15,39 +16,44 @@ var DEVMODE = true;
 var DST     = 'tagged/';
 
 
-gulp.task('default', ['sass', 'coffee', 'jade', 'icon'], function(){
+gulp.task('default', ['sass', 'coffee', 'pug', 'yaml', 'icon'], function(){
     gulp.watch('scss/**/*.scss', ['sass']);
     gulp.watch('coffee/**/*.coffee', ['coffee']);
-    gulp.watch('jade/**/*.jade', ['jade']);
+    gulp.watch('pug/**/*.jade', ['pug']);
+    gulp.watch('./**/*.yaml', ['yaml']);
     gulp.watch('svg/icons/**/*.svg', ['icon']);
 });
 
-gulp.task('dist', function(){
+gulp.task('dist', function(done){
     DEVMODE = false;
     gulp.start('build');
+    done();
 });
 
-gulp.task('build', ['sass', 'coffee', 'jade', 'png', 'icon']);
+gulp.task('build', ['sass', 'coffee', 'pug', 'png', 'yaml', 'icon']);
 
 gulp.task('sass', function() {
     return gulp.src('scss/*.scss')
-        .pipe(sass({ errLogToConsole: true })) .on('error', gutil.log)
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: DEVMODE ? 'compact' : 'compressed'
+        })) .on('error', dealWithError)
         .pipe(gulp.dest(DST));
 });
 
 gulp.task('coffee', function() {
     return gulp.src('coffee/*.coffee')
         .pipe(DEVMODE ? sourcemaps.init() : gutil.noop())
-        .pipe(coffee({ bare: false })) .on('error', gutil.log)
+        .pipe(coffee({ bare: false })) .on('error', dealWithError)
         .pipe(DEVMODE ? sourcemaps.write() : gutil.noop())
         .pipe(ngAnnotate())
-        .pipe(DEVMODE ? gutil.noop() : uglify()) .on('error', gutil.log)
+        .pipe(DEVMODE ? gutil.noop() : uglify()) .on('error', dealWithError)
         .pipe(gulp.dest(DST));
 });
 
-gulp.task('jade', function() {
-    return gulp.src('jade/*.jade')
-        .pipe(jade({
+gulp.task('pug', function() {
+    return gulp.src('pug/*.jade')  //oops!
+        .pipe(pug({
             locals: {},
             client: false,
             pretty: DEVMODE,
@@ -55,18 +61,29 @@ gulp.task('jade', function() {
         .pipe(gulp.dest(DST));
 });
 
+gulp.task('yaml', function(){
+    return gulp.src('**/*.yaml')
+        .pipe(yaml({
+            space: DEVMODE ? 4 : 0
+        }))
+        .pipe(gulp.dest(DST));
+});
+
 gulp.task('png', function(done){
     var files = sh.ls('svg/2png/*.svg');
     var count = 0;
+    var aux = function(){ if(!--count) done(); };
     for(var i = 0; i < files.length; i++){
+        var dst = DST; //'www/img/' + (/([a-zA-Z_-]+)\.svg$/.exec(files[i]))[1] + '/';
+        sh.mkdir('-p', dst);
         count++;
         svgAux({
-            done      : function(){ if(!--count) done(); },
+            done      : aux,
             input     : files[i],
-            output    : DST,
+            output    : dst,
             valid     : /xx\w+/,
             cut       : 2,
-            cmd       : '-e ',
+            cmd       : '-e ',  //NOTE manter o espaco do final!
             extension : '.png',
             replace   : [{from: '#f2f2f2', to: 'none'}],
         });
@@ -84,43 +101,62 @@ gulp.task('icon', function(){
         .pipe(gulp.dest(DST));
 });
 
+
 function svgAux(ops){
     if(!sh.which('inkscape')){
         gutil.log('OOps! Inkscape must be in your path');
     }
     else{
-        var tmp = Math.random().toString(36).substr(7) + '.svg'
+        var tmp = Math.random().toString(36).substr(7) + '.svg';
         sh.cp(ops.input, tmp);
         for(var i = 0; i < ops.replace.length; i++){
             sh.sed('-i', ops.replace[i].from, ops.replace[i].to, tmp);
         }
         sh.exec('inkscape -z -S ' + tmp, {silent: true}, function(code, data){
-            var name = /^([^\n,]+)/gm;
-            var m, count = 0;
+            var objId = /^([^\n,]+)/gm;
+            var m, count = 0, total = 0, total2 = 0;
+
+            var aux = function doit(obj){
+                if(count >= 3){
+                    setTimeout(function(){ doit(obj); }, 300);
+                    return;
+                }
+                count++;
+                var name = obj.substring(ops.cut);
+                console.log('inkscape -z -d 90 ' + ops.cmd + ops.output + name + ops.extension + ' -j -i ' + obj + ' ' + tmp);
+                sh.exec('inkscape -z -d 90 ' + ops.cmd + ops.output + name + ops.extension + ' -j -i ' +
+                                                        obj + ' ' + tmp, {silent: true}, function(){
+                    count--;
+                    if(!--total){
+                        gutil.log('Exportados ' + total2 + ' arquivos para: ' + ops.output);
+                        sh.rm(tmp);
+                        ops.done();
+                    }
+                });
+            };
 
             do{
-                m = name.exec(data);
+                m = objId.exec(data);
                 if(m){
                     if(ops.valid.test(m[1])){
-                        (function(name){
-                            count++;
-                            sh.exec('inkscape -z -d 90 ' + ops.cmd + ops.output + name + ops.extension + ' -j -i ' +
-                                                                    m[1] + ' ' + tmp, {silent: true}, function(){
-                                gutil.log('Extracted: ' + ops.output + name + ops.extension);
-                                if(!--count){
-                                    sh.rm(tmp);
-                                    ops.done();
-                                }
-                            });
-                        })(m[1].substring(ops.cut));
+                        total++;
+                        total2++;
+                        aux(m[1]);
                     }
                 }
             } while(m);
 
-            if(!count){
+            if(!total){
                 sh.rm(tmp);
                 ops.done();
             }
         });
     }
+}
+
+
+function dealWithError(error){
+    gutil.log(error.toString());
+    gutil.beep();
+    this.emit('end');
 }
